1d09d1e576b640db484d603dd2f2cafda6bad0d6
EveSunMaple <evesunmaple@outlook.com>
2025-08-10 23:21:09 +0800

    feat: Add rounded styling to alert component and include warning message in blog post

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+export default {
+  safelist: [
+    "alert",
+    "alert-info",
+    "alert-success",
+    "alert-warning",
+    "alert-error",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
