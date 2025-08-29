e30b7ff809a12f157479b895c4f9a2a26560c54e
EveSunMaple <evesunmaple@outlook.com>
2025-08-29 14:55:50 +0800

    feat: Replace custom code styling with expressive-code plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import vercel from "@astrojs/vercel";
 import compress from "@playform/compress";
 import terser from "@rollup/plugin-terser";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
+
 import remarkMath from "remark-math";
 
 // https://astro.build/config
@@ -19,177 +21,8 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx(), react(), sitemap(), compress(), terser({ compress: true, mangle: true }), icon()],
+  integrations: [expressiveCode(), mdx(), react(), sitemap(), compress(), terser({ compress: true, mangle: true }), icon()],
   markdown: {
-    shikiConfig: {
-      themes: {
-        light: "github-light",
-        dark: "github-dark",
-      },
-      transformers: [{
-        preprocess(code, options) {
-          this.meta = { lang: options.lang || "plaintext" };
-          return code;
-        },
-        pre(node) {
-          const language = this.meta?.lang.toUpperCase() || "plaintext";
-
-          return {
-            type: "element",
-            tagName: "div",
-            properties: {
-              class: "not-prose saro-code",
-            },
-            children: [
-              {
-                type: "element",
-                tagName: "div",
-                properties: {
-                  class: "saro-code-toolbar",
-                },
-                children: [
-                  {
-                    type: "element",
-                    tagName: "span",
-                    properties: { class: "saro-code-toolbar-language" },
-                    children: [{ type: "text", value: language }],
-                  },
-                  {
-                    type: "element",
-                    tagName: "button",
-                    properties: {
-                      "class": "btn-copy",
-                      "aria-label": "Copy code",
-                      "type": "button",
-                    },
-                    children: [
-                      {
-                        type: "element",
-                        tagName: "span",
-                        properties: {
-                          "class": "saro-code-toolbar-copy-icon",
-                          "aria-hidden": "true",
-                        },
-                        children: [
-                          {
-                            type: "element",
-                            tagName: "svg",
-                            properties: {
-                              "xmlns": "http://www.w3.org/2000/svg",
-                              "width": "18",
-                              "height": "18",
-                              "viewBox": "0 0 24 24",
-                              "fill": "none",
-                              "stroke": "currentColor",
-                              "stroke-width": "2",
-                              "stroke-linecap": "round",
-                              "stroke-linejoin": "round",
-                              "class": "copy-icon",
-                            },
-                            children: [
-                              {
-                                type: "element",
-                                tagName: "rect",
-                                properties: {
-                                  x: "9",
-                                  y: "9",
-                                  width: "13",
-                                  height: "13",
-                                  rx: "2",
-                                  ry: "2",
-                                },
-                                children: [],
-                              },
-                              {
-                                type: "element",
-                                tagName: "path",
-                                properties: {
-                                  d: "M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1",
-                                },
-                                children: [],
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                      {
-                        type: "element",
-                        tagName: "span",
-                        properties: {
-                          "class": "saro-code-toolbar-copy-success hidden",
-                          "aria-hidden": "true",
-                        },
-                        children: [
-                          {
-                            type: "element",
-                            tagName: "svg",
-                            properties: {
-                              "xmlns": "http://www.w3.org/2000/svg",
-                              "width": "18",
-                              "height": "18",
-                              "viewBox": "0 0 24 24",
-                              "fill": "none",
-                              "stroke": "currentColor",
-                              "stroke-width": "2",
-                              "stroke-linecap": "round",
-                              "stroke-linejoin": "round",
-                              "class": "success-icon",
-                            },
-                            children: [
-                              {
-                                type: "element",
-                                tagName: "path",
-                                properties: {
-                                  d: "M20 6L9 17l-5-5",
-                                },
-                                children: [],
-                              },
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                ...node,
-                properties: {
-                  ...node.properties,
-                  class: "saro-code-content",
-                },
-                children: [
-                  {
-                    type: "element",
-                    tagName: "code",
-                    properties: {
-                      class: "grid [&>.line]:px-4",
-                      style: "counter-reset: line",
-                    },
-                    children: node.children,
-                  },
-                ],
-              },
-            ],
-          };
-        },
-        line(node) {
-          return {
-            ...node,
-            properties: {
-              ...node.properties,
-              class: "line before:content-[counter(line)]",
-              style: "counter-increment: line",
-            },
-          };
-        },
-        code(node) {
-          delete node.properties.style;
-          return node;
-        },
-      },
-      ],
-    },
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
